246bb49f053a5c2e9f9ce1d4e7d1b4106a18ad34
SrgHauser <96929031+SrgHauser@users.noreply.github.com>
2021-12-31 22:02:29 -0300

    Add files via upload

---
+++ ASCII.java
@@ -0,0 +1,16 @@
+//Tabela ASCII 0-255
+public class ASCII {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int exemplo = 255, cont = 0; 
+		
+		while(exemplo >= cont) {	
+		System.out.println(cont + " " + (char)cont);
+		cont++;
+		
+		}
+	}
+
+}
